71e6bdd3c55778d3c475a15e9002066f6dc715ad
Devansh051 <devanshagrawal@gamil.com>
2025-04-17 21:56:56 +0530

    Enhanced Team section with Framer Motion animations

--- src/data/Teamdata.jsx
+++ src/data/Teamdata.jsx
@@ -3,46 +3,73 @@ export const TeamDetails = [
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
         role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (1).svg",
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
-    {
-        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
-        name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (2).svg",
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
-    {
-        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
-        name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (3).svg"
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
-    {
-        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
-        name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (13).svg"
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (5).svg"
+    }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (6).svg"
+    }, 
+    {
+        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
+        name: "Akhilesh Pachnanda",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (7).svg"
+    }, 
+    {
+        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
+        name: "Akhilesh Pachnanda",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (12).svg"
+    }, 
+    {
+        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
+        name: "Akhilesh Pachnanda",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (9).svg"
     }, 
     
 ];
@@ -52,48 +79,74 @@ export const TeamDetails1 = [
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
         role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (1).svg",
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
-    {
-        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
-        name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (2).svg",
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
-    {
-        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
-        name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (3).svg"
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
-    {
-        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
-        name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (13).svg"
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (5).svg"
+    }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (6).svg"
     }, 
-    
+    {
+        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
+        name: "Akhilesh Pachnanda",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (7).svg"
+    }, 
+    {
+        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
+        name: "Akhilesh Pachnanda",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (12).svg"
+    }, 
+    {
+        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
+        name: "Akhilesh Pachnanda",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (9).svg"
+    }, 
 ];
 
 export const TeamDetails2 = [
@@ -101,45 +154,72 @@ export const TeamDetails2 = [
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
         role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (1).svg",
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
-    {
-        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
-        name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (2).svg",
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
-    {
-        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
-        name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (3).svg"
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
-    {
-        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
-        name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (13).svg"
     }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (5).svg"
+    }, 
     {
         img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
         name: "Akhilesh Pachnanda",
-        role: "Founder Founder",
-    },
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (6).svg"
+    }, 
+    {
+        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
+        name: "Akhilesh Pachnanda",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (7).svg"
+    }, 
+    {
+        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
+        name: "Akhilesh Pachnanda",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (12).svg"
+    }, 
+    {
+        img: "src/assets/charlie-green-3JmfENcL24M-unsplash.jpg",
+        name: "Akhilesh Pachnanda",
+        role: "Founder, Founder",
+        linkedin: "https:// link",
+       instagram: "https: google",
+       star_icon:"src/assets/ISoC - SVG_Elements (1)/ISoC - SVG (9).svg"
+    }, 
 ];
