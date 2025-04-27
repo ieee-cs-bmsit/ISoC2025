7846d00855a7240c21db838bce7d5cccee8262f0
PrithwisK07 <karmakarprithwis566@gmail.com>
2025-04-27 20:36:33 +0530

    hero bg fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,67 +1,65 @@
-import React from 'react'
-import heroLogo from '../assets/img/socbiegelogo.png'
+import React, { useState, useEffect } from 'react'
+import heroBg from '../assets/img/ISoc_Desktop_Hero.svg'
+import mobileLogo from '../assets/img/ISoc_Mobile_Hero.svg'
 import "./Hero.css"
-import heroBg from '../assets/img/2.svg'
 
 const Hero = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 640);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <section
-            className="h-screen w-screen text-white flex flex-col font-body"
-        >
+        <section className="hero-img-container lg:h-screen md:h-[60vh] h-[65vh] w-screen text-white flex flex-col font-body relative">
+            <div className="flex-grow flex flex-col lg:justify-end justify-end items-center text-center relative">
 
-            <div className="flex-grow flex flex-col lg:justify-end justify-end items-center text-center">
-                <img src={heroBg} alt=""
-                    className='w-full h-full absolute'
+                {/* Logo: Switch between 2.svg (desktop) and ISoc_Mobile_Hero.svg (mobile) */}
+                <img
+                    src={isMobile ? mobileLogo : heroBg}
+                    alt="Hero Logo"
+                    className="w-full h-full absolute top-0 left-0 object-contain z-10"
                 />
-                {/* <img src={heroLogo} alt="IEEE Summer of Code" className="max-w-[90%] md:max-w-lg lg:scale-150 md:scale-120 sm:scale-100
-                sm:-mt-15 lg:mt-25" /> */}
 
-                <div className="w-1/2 justify-around flex space-x-6 -ml-8 relative -top-5">
+                {/* Buttons */}
+                <div className="w-1/2 justify-around flex space-x-6 -ml-8 relative -top-5 z-20">
                     <button
                         className="relative py-3 lg:py-4 font-bold text-black bg-white cursor-pointer 
-        px-6 sm:px-8 md:px-6 lg:px-10 z-1
-        shadow-[8px_8px_0px_-2px_rgba(235,_210,_47,_1)] border-2 border-black"
+                        px-6 sm:px-8 md:px-6 lg:px-10
+                        shadow-[8px_8px_0px_-2px_rgba(235,_210,_47,_1)] border-2 border-black"
                     >
                         <p className='text-xs sm:text-lg'>
-                            <a href="https://unstop.com/p/ieee-summer-of-code-bms-insitute-of-technology-and-management-1469982" target='_blank'>REGISTER</a>
+                            <a href="https://unstop.com/p/ieee-summer-of-code-bms-insitute-of-technology-and-management-1469982" target='_blank' rel="noopener noreferrer">
+                                REGISTER
+                            </a>
                         </p>
                     </button>
 
                     <button
                         className="relative py-3 lg:py-4 font-bold text-black bg-white cursor-pointer 
-        px-6 sm:px-8 md:px-6 lg:px-10 
-        shadow-[8px_8px_0px_-2px_rgba(235,_210,_47,_1)] border-2 border-black"
+                        px-6 sm:px-8 md:px-6 lg:px-10
+                        shadow-[8px_8px_0px_-2px_rgba(235,_210,_47,_1)] border-2 border-black"
                     >
                         <p className='text-xs sm:text-lg'>ABOUT</p>
                     </button>
                 </div>
             </div>
+
+            {/* Marquee */}
             <div className="marquee-text lg:text-xl sm:text-lg text-md text-white font-body font-bold bg-[#1f3bfb]">
                 <div className="marquee-text-track">
-                    <p>BMSITM</p>
-                    <p>✺</p>
-                    <p>9th May to 28st June</p>
-                    <p>✺</p>
-                    <p>IEEE CS BMSITM</p>
-                    <p>✺</p>
-                    <p>BMSITM</p>
-                    <p>✺</p>
-                    <p>9th May to 28st June</p>
-                    <p>✺</p>
-                    <p>IEEE CS BMSITM</p>
-                    <p>✺</p>
-                    <p aria-hidden="true">BMSITM</p>
-                    <p aria-hidden="true">✺</p>
-                    <p aria-hidden="true">9th May to 28st June</p>
-                    <p aria-hidden="true">✺</p>
-                    <p aria-hidden="true">IEEE CS BMSITM</p>
-                    <p aria-hidden="true">✺</p>
-                    <p aria-hidden="true">BMSITM</p>
-                    <p aria-hidden="true">✺</p>
-                    <p aria-hidden="true">9th May to 28st June</p>
-                    <p aria-hidden="true">✺</p>
-                    <p aria-hidden="true">IEEE CS BMSITM</p>
-                    <p aria-hidden="true">✺</p>
+                    <p>BMSITM</p><p>✺</p><p>9th May to 28st June</p><p>✺</p><p>IEEE CS BMSITM</p><p>✺</p>
+                    <p>BMSITM</p><p>✺</p><p>9th May to 28st June</p><p>✺</p><p>IEEE CS BMSITM</p><p>✺</p>
+                    <p aria-hidden="true">BMSITM</p><p aria-hidden="true">✺</p><p aria-hidden="true">9th May to 28st June</p><p aria-hidden="true">✺</p><p aria-hidden="true">IEEE CS BMSITM</p><p>✺</p>
+                    <p aria-hidden="true">BMSITM</p><p aria-hidden="true">✺</p><p aria-hidden="true">9th May to 28st June</p><p aria-hidden="true">✺</p><p aria-hidden="true">IEEE CS BMSITM</p><p>✺</p>
                 </div>
             </div>
         </section>
