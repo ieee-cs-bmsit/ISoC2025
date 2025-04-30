5e53c8e6e7e9a3594faa04a26947f7a897241832
Arnabpaul0101 <arnabpaul01012004@gmail.com>
2025-04-30 18:47:49 +0530

    new sponsors

--- src/components/WhyParticipate.jsx
+++ src/components/WhyParticipate.jsx
@@ -1,38 +1,9 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import reasons from '../data/Whycontributecards';
 
 const WhyParticipate = () => {
 
-  const reasons = [
-    {
-      title: 'Prize Pool : Rs.50,000',
-      description: (
-        <>
-          <p>FREE Registrations</p>
-          <p>Compete for a chance to win cash prizes.</p>
-          
-          <p>Boost your portfolio with impactful contributions.</p>
-        </>
-      )
-      ,
-    },
-    {
-      title: 'Swags & Recognition',
-      description:
-        'Goodie Boxes for top 50 participants! Earn cool swags, and showcase your work to the community and potential recruiters.',
-    },
-    {
-      title: 'Real-World Impact',
-      description:
-        'Contribute to meaningful open-source projects that solve real problems and benefit the global community.',
-    },
-    {
-      title: 'Collaborate & Network',
-      description:
-        'Meet passionate developers, designers, and innovators from all over. Build connections that last beyond the event.',
-    },
-    
-  ];
 
   return (
     <section id="why-participate" className="py-20 px-4 bg-[#f4611d]">
---
+++ src/data/Whycontributecards.jsx
@@ -0,0 +1,32 @@
+const reasons = [
+    {
+      title: 'Prize Pool : Rs.50,000',
+      description: (
+        <>
+          <p className=" text-lg md:text-xl font-semibold">FREE Registrations</p>
+          <p>Compete for a chance to win cash prizes.</p>
+          
+          <p>Boost your portfolio with impactful contributions.</p>
+        </>
+      )
+      ,
+    },
+    {
+      title: 'Swags & Recognition',
+      description:
+        'Goodie Boxes for top 50 participants! Earn cool swags, and showcase your work to the community and potential recruiters.',
+    },
+    {
+      title: 'Real-World Impact',
+      description:
+        'Contribute to meaningful open-source projects that solve real problems and benefit the global community.',
+    },
+    {
+      title: 'Collaborate & Network',
+      description:
+        'Meet passionate developers, designers, and innovators from all over. Build connections that last beyond the event.',
+    },
+    
+  ];
+
+  export default reasons;
